2ca8531f18d21329b2bac6bdb9a9596c00932294
ntk-y <69511628+ntk-y@users.noreply.github.com>
2020-10-26 12:50:29 +0900

    Update divided_by_tow.py

--- divided_by_tow.py
+++ divided_by_tow.py
@@ -1,4 +1,7 @@
-# ミニマムニューロン #
+"""
+入力した数を２で割るだけ
+ex→ 入力：100　結果：49.172684
+"""
 
 import keras
 from keras.models import Sequential
@@ -12,7 +15,6 @@ model.add( InputLayer( input_shape= (1,) ) )
 model.add( Dense( units= 1, activation='linear') ) # units(層内のニューロンの数) activation(活性化関数)
 # 重みを探す方法（＝最適化法）を指定 optimizersはモジュール
 sgd = optimizers.SGD( learning_rate=0.001 ) # learning_rate(学習率)の設定は大切！
-# sgd = optimizers.SGD( learning_rate=0.000000000000000001 ) # learning_rate(学習率)の設定は大切！
 model.compile( loss = 'mse',optimizer = sgd, metrics =["accuracy"]) # 二乗平均誤差
 
 #概略表示
@@ -25,8 +27,7 @@ history = model.fit( train_x0, train_x1, epochs = 50 )
 
 #実用テスト
 num1 = input()
-num2 = input()
 # test_x0 = numpy.array( [ 100, 200 ] )
-test_x0 = numpy.array( [ int(num1) ,int(num2) ] )
+test_x0 = numpy.array( [ int(num1) ] )
 res = model.predict( test_x0 )
 print( res )
